d83ad29308698fd4c6379fe59feb290961dafdd5
Huabin Liu <hbliu@thoughtworks.com>
2020-07-01 00:29:42 +0800

    pass test

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,4 @@
-const parseData
+const parseData = (input) => {
+	return input.data.map((e, i) => e.reduce((a, v, i) => Object.assign(a, { [input.column[i].name]: v }), {}));
+};
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,5 @@
-const inject
+const inject = (items, sections) => {
+	sections.forEach((e, i) => items.splice(e.index+i, 0, e.content));
+	return items;
+};
 export { inject };
